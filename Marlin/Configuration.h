5dbdd2a0feded2743d6ea99d1c0c056595a01dae
Michael Earls <me@cerkit.com>
2020-07-05 15:04:50 -0500

    Updated PID for MicroSwiss all metal hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -491,9 +491,14 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  // #define DEFAULT_Kp 21.73
+  // #define DEFAULT_Ki 1.54
+  // #define DEFAULT_Kd 76.55
+
+  // MicroSwiss All Metal Hotend
+  #define DEFAULT_Kp 31.80
+  #define DEFAULT_Ki 3.63
+  #define DEFAULT_Kd 69.68
 
 #endif // PIDTEMP
 
